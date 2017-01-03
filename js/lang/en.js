c0bb6a82bc84b2f49e36ca21dfab1533f932378f
godmeir <godmeir@126.com>
2017-01-03 14:20:44 +0800

    init: scroll、pick

---
+++ js/lang/en.js
@@ -0,0 +1,105 @@
+var lang={
+	//模式
+	mode_bzz:"Standard",
+	mode_jhz:"Refined",
+	mode_ckz:"Fast",
+	mode_zz:"Porridge",
+	mode_xf:"Conjee",
+	mode_bt:"Soup",
+	mode_zjr:"Reheat",
+	mode_zlz:"CGporridge",
+	mode_gbf:"Pot rice",
+	mode_zhz:"Stew",
+	mode_dg:"Cake",
+	mode_sn:"Yoghurt",
+	mode_bw:"Keep warm",
+
+	//米种
+	rice_kind:"Rice kind",
+	long_rice:"Long",
+	short_rice:"Short",
+	cao_rice:"Coarse",
+
+	//口感
+	taste:"Taste",
+	soft:"Softer",
+	suitable:"Medium",
+	hard:"Harder",
+
+	//阶段
+	stage_one:"Preheat",
+	stage_two:"Water absorption",
+	stage_three:"Heat",
+	stage_four:"Boiling",
+	stage_five:"Braised rice",
+
+	//可调时间范围
+	time_zhuzhou:"Adjustment time rang: 1 to 2 h",
+	time_baotang:"Adjustment time rang:1.5 to 3 h",
+	time_zhengzhu:"Adjustment time rang:10 min to 1 h",
+	time_suannai:"Adjustment time rang:6 to 8 h",
+	time_xifan:"Adjustment time rang:40 min to 1.5 h",
+	time_zaliangzhou:"Adjustment time rang:1 to 3 h",
+    //文案
+	Txt_Regular:"Regular time to choose:",
+	Txt_Preset:"Preset time to finish:",
+
+	//标题编辑按钮
+	title_editor:"Editor",
+	//底部按钮
+	btnFunc:"Func",
+	btnPreset:"Preset",
+	btnMenu:"Menu",
+	btnVoice:"Voice",
+	//save按钮
+	btnSave:"Save",
+	//开关
+	txtPow:"Turn on",
+	//时间单位
+	unitHour: "H",
+	unitMin:"Min",
+	//按钮
+	btnStart:"Start",
+	btnCancel:"Cancel",
+	btnAffirm:"Affirm",
+
+	//状态
+	txtLast:"Remain",
+	presetState:"Presetting...",
+	//提示
+	txtNoPreset:"Current mode can’t make an appointment,Thanks！",
+	txtMakeSure:"Cancel cooking during cooking process will affect the cooking effect. Please  confirm cancel cooking or not? ",
+
+	//故障信息
+	Err_titleR:"Cause:",
+	Err_titleP:"Troubleshooting:",
+
+	Err_E0R:"The public power grid voltage is too low.",
+	Err_E01P:"The cooker will automatically quit the mode after the public power grid becomes normal.",
+	Err_E01P1:"If the error still cannot be eliminated, please send the product to designated maintenance station.",
+
+	Err_E1R:"The public power grid voltage is too high.",
+
+	Err_E4R:"IGBT temperature sensor is short-circuited or open-circuited.",
+	Err_E456Ad08P:"After the error is eliminated, it will automatically quit the error mode.",
+	Err_E456Ad08P1:"If it cannot quit the error mode for long, or it cannot be eliminated after re-energizing the cooker, please send the product to designated maintenance station.",
+
+	Err_E5R:"The upper cover temperature sensor is short-circuited or open-circuited.",
+
+	Err_E6R:"The pot bottom temperature sensor is short-circuited or open-circuited.",
+
+	Err_EAR:"IGBT temperature sensor conducts overheat protection.",
+
+	Err_EdR:"The pot bottom temperature sensor conducts overheat protection.",
+
+	Err_EwuR:"There is no inner pot inside or the electric circuit is faulted.",
+	Err_EwuP:"If the inner pot has not been put inside, after putting the inner pot, press “heat insulation/cancel” button to quit the error alarming status, and return to the stand-by interface.",
+	Err_EwuP1:"If the inner pot has been put inside, and the error still cannot be eliminated, please send the product to designated maintenance station.",
+
+	Err_H0R:"Communication between display board and mainboard is faulted.",
+
+	Err_H8R:"Communication between main control chip and touch chip is faulted.",
+
+	Err_Multi:"Current device occurred multiple errors,",
+	Err_P:"Click the button to see the details."
+}
---
+++ js/lang/zh_CN.js
@@ -0,0 +1,106 @@
+var lang={
+	//模式
+	mode_bzz:"标准煮",
+	mode_jhz:"精华煮",
+	mode_ckz:"超快煮",
+	mode_zz:"煮粥",
+	mode_xf:"稀饭",
+	mode_bt:"煲汤",
+	mode_zjr:"再加热",
+	mode_zlz:"杂粮粥",
+	mode_gbf:"锅巴饭",
+	mode_zhz:"蒸煮",
+	mode_dg:"蛋糕",
+	mode_sn:"酸奶",
+	mode_bw:"保温",
+
+	//米种
+	rice_kind:"米种",
+	long_rice:"长米粒",
+	short_rice:"短米粒",
+	cao_rice:"糙米",
+
+	//口感
+	taste:"口感",
+	soft:"稍软",
+	suitable:"适中",
+	hard:"稍硬",
+
+	//阶段
+	stage_one:"预热",
+	stage_two:"吸水",
+	stage_three:"加热",
+	stage_four:"沸腾",
+	stage_five:"焖饭",
+
+	//可调时间范围
+	time_zhuzhou:"可调时间范围：1~2小时",
+	time_baotang:"可调时间范围：1.5~3小时",
+	time_zhengzhu:"可调时间范围：10分钟~1小时",
+	time_suannai:"可调时间范围：6~8小时",
+	time_xifan:"可调时间范围：40分钟~1.5小时",
+	time_zaliangzhou:"可调时间范围：1~3小时",
+
+	Txt_Regular:"定时时间选择:",
+	Txt_Preset:"预约完成时间:",
+
+	//标题编辑按钮
+	title_editor:"编辑",
+	//标题编辑按钮
+	title_editor:"编辑",
+	//底部按钮
+	btnFunc:"功能",
+	btnPreset:"预约",
+	btnMenu:"菜谱",
+	btnVoice:"语音",
+	//save按钮
+	btnSave:"保存",
+	//开关
+	txtPow:"开机",
+	//时间单位
+	unitHour: "小时",
+	unitMin:"分钟",
+	//按钮
+	btnStart:"开始",
+	btnCancel:"取消",
+	btnAffirm:"确定",
+	//状态
+	txtLast:"剩余",
+	presetState:"预约中...",
+	//提示
+	txtNoPreset:"当前模式不得进行预约，谢谢！",
+	txtMakeSure:"烹饪进行中，如果中途取消烹饪工作会影响烹饪效果。是否确定取消!",
+
+	//故障信息
+	Err_titleR:"故障原因:",
+	Err_titleP:"解决条件:",
+
+	Err_E0R:"市电电压过低。",
+	Err_E01P:"市电电压正常后自动退出。",
+	Err_E01P1:"若电压正常后仍出现，请送维修点维修。",
+
+	Err_E1R:"市电电压过高。",
+
+	Err_E4R:"IGBT温度传感器短路或开路",
+	Err_E456Ad08P:"故障恢复后自动退出。",
+	Err_E456Ad08P1:"若长时间不退出，重新上电后仍出现，请送维修点维修。",
+
+	Err_E5R:"上盖温度传感器短路或开路。",
+
+	Err_E6R:"锅底温度传感器短路或开路。",
+
+	Err_EAR:"IGBT温度传感器过温保护。",
+
+	Err_EdR:"锅底温度传感器过温保护。",
+
+	Err_EwuR:"无内锅工作或电路故障。",
+	Err_EwuP:"若没有放入配锅，则放入配锅后，按“保温/取消”键退出，若没有放入配锅，则放入配锅后，按“保温/取消”键退出报警状态，返回待机界面。",
+	Err_EwuP1:"若已放入配锅而出现异常，请送维修点维修。",
+
+	Err_H0R:"显示板与主板间通讯异常。",
+
+	Err_H8R:"主控芯片与触摸芯片间通讯异常。",
+
+	Err_Multi:"当前设备发生了多个故障，",
+	Err_P:"点击按钮后可查看故障详情"
+}
